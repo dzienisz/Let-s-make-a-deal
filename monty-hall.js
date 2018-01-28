7d0ac38e34130ab584d24266f3db694aab543e5a
Kamil Dzieniszewski <kamil.dzieniszewski@gogoapps.io>
2018-01-28 21:51:54 +0100

    Initial commit

---
+++ monty-hall.js
@@ -0,0 +1,77 @@
+const readline = require('readline');
+var colors = require('colors/safe');
+
+// deklaracja tablicy
+let nagroda = new Array(3).fill(false);
+
+// readline config
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// ukryj nagrode
+const random = Math.floor((Math.random() * 3));
+nagroda[random] = true;
+
+// Intro
+ 
+console.log(colors.inverse('                     ')); 
+console.log(colors.inverse('    Witaj w grze!    ')); 
+console.log(colors.inverse('                     ')); 
+console.log(colors.inverse('    Idź na całość    ')); 
+console.log(colors.inverse('                     ')); 
+console.log('                     '); 
+console.log(colors.inverse('                     ')); 
+console.log(colors.inverse('    Przed sobą       ')); 
+console.log(colors.inverse('    widzisz 3 drzwi  ')); 
+console.log(colors.inverse('                     ')); 
+console.log('                     '); 
+console.log(colors.inverse('                     ')); 
+console.log(colors.inverse('    Za jednymi       '));
+console.log(colors.inverse('    z nich           '));
+console.log(colors.inverse('    czeka...         '));
+console.log(colors.inverse('                     ')); 
+console.log(colors.inverse(colors.rainbow('    Nagroda!!!       ')));
+console.log(colors.inverse('                     ')); 
+console.log('                     '); 
+console.log('Które drzwi wybierasz?'); 
+console.log('1, 2 a może 3?'); 
+console.log('                     '); 
+
+
+// rl.question('Please enter the first number : ', (answer1) => {
+//     rl.question('Please enter the second number : ', (answer2) => {
+//         var result = (+answer1) + (+answer2);
+//         console.log(`The sum of above two numbers is ${result}`);
+//         rl.close();
+//     });
+// });
+
+//losowanie
+rl.question('--> ', (answer) => {
+    if (nagroda[(answer-1)] === true) {
+        console.log(colors.inverse('                     ')); 
+        console.log(colors.inverse(colors.rainbow('    Wygrałeś!!!      ')));
+        console.log(colors.inverse('                     ')); 
+        rl.close();
+    } else {
+        console.log("Bramka nr: " + answer + " okazała się pusta!")
+        console.log("")
+
+        //tutaj skonczyłem kod
+
+        rl.question('--> ', (answer2) => {
+            if (nagroda[(answer2-1)] === true) {
+                console.log(colors.inverse('                     ')); 
+                console.log(colors.inverse(colors.rainbow('    Wygrałeś!!!      ')));
+                console.log(colors.inverse('                     ')); 
+                rl.close();
+            } else {
+                console.log("Nie wygrałeś");
+                rl.close();
+            }
+        });   
+    }
+});
+
